2d5f2be357342798b09baa860fd57bdfcfc44d4f
Robert Fletcher <lobatifricha@gmail.com>
2025-12-28 23:27:41 -0500

    add Stylelint plugins (#1294)

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,10 @@
 export default {
   "extends": [
+    "stylelint-config-property-sort-order-smacss",
     "stylelint-config-standard",
+    "stylelint-selector-bem-pattern",
+    "./.stylelint_wishlist.yml",
     "./.stylelint_todo.yml",
   ],
+  plugins: ["stylelint-selector-bem-pattern"],
 };
