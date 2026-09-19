efce85a6420dfc9aa4c5e937dac92f1d0e5b12ca
Robert Fletcher <lobatifricha@gmail.com>
2026-09-19 16:58:04 -0700

    Revert "introduce GoodJob (#1490)"
    **What**
    
    This reverts commit ad7501e36cc25c3fa40979ad128bbc1d72fd7e35.
    
    **Why**
    
    Changed my mind. Going to go with Solid Queue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,8 @@ Rails.application.configure do
   # Replace the default in-process memory cache store with a durable alternative.
   # config.cache_store = :mem_cache_store
 
-  # Run jobs in the separate worker process declared in the Procfile.
-  config.good_job.execution_mode = :external
+  # Replace the default in-process and non-durable queuing backend for Active Job.
+  # config.active_job.queue_adapter = :resque
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
